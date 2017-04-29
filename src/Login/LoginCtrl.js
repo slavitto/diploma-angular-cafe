3fe77574b62c4e3e9ff917669dd63a6c25959518
Slava Siomichev <slavitto@gmail.com>
2017-04-29 10:53:12 +0400

    procfile

--- src/Login/LoginCtrl.js
+++ src/Login/LoginCtrl.js
@@ -15,3 +15,15 @@ DroneCafeApp
   }
   
 });
+// app.factory('Session', function($http) {
+//   var Session = {
+//     data: {},
+//     saveSession: function() { /* save session data to db */ },
+//     updateSession: function() { 
+//       /* load data from db */
+//       $http.get('session.json').then(function(r) { return Session.data = r.data;});
+//     }
+//   };
+//   Session.updateSession();
+//   return Session; 
+// });
