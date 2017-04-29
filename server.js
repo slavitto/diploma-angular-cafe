fc11a888c92140a4bfa6ca90caeab44cc1a0346e
Slava Siomichev <slavitto@gmail.com>
2017-04-29 17:33:34 +0400

    no-router-heroku-port-error-code-added

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-// app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 3000));
 
 //For avoidong Heroku $PORT error
 app.get('/', function(request, response) {
     var result = 'App is running'
     response.send(result);
 }).listen(app.get('port'), function() {
-    console.log('App is running, server is listening on port ', app.get(5000));
+    console.log('App is running, server is listening on port ', app.get('port'));
 });
 
 var mongooseService = require("./src/Service/MongooseService");
