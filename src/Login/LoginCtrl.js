e73f3dfa29c19bb7a5d2a5eae75b89262f00c2d4
Slava Siomichev <slavitto@gmail.com>
2017-04-30 23:59:41 +0400

    re-login-orders-fixed-again

--- src/Login/LoginCtrl.js
+++ src/Login/LoginCtrl.js
@@ -7,16 +7,17 @@ DroneCafeApp
 
         if (cookie !== undefined) {
             socket.emit('logOut', cookie);
-            cookie.orders = cookie.orders.filter(function(order){
-                return (order.state === "ordered" || order.state === "cooking");
-            }) || [];
+            if (cookie.orders)
+                cookie.orders = cookie.orders.filter(function(order) {
+                    return (order.state === "ordered" || order.state === "cooking");
+                });
             $cookies.putObject('customer', cookie);
         }
 
         $scope.login = function(newUser) {
             socket.emit('newUser', newUser);
             socket.on('newCustomer', function(newCustomer) {
- 				if (!cookie) $cookies.putObject('customer', newCustomer);
+                if (!cookie) $cookies.putObject('customer', newCustomer);
                 $window.location.href = '/';
             });
         }
